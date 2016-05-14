018e644df0567e8bf188bfbccb0a78d0b06e4bd8
ccleung <clemsquared@gmail.com>
2016-05-14 17:10:30 -0400

    get fb demo to work

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -9,18 +9,20 @@ class FacebookController < ActionController::Base
     messaging_events = params[:entry][0][:messaging]
     messaging_events.each do |msg_event|
       sender = msg_event[:sender][:id]
-      unless msg_event[:message].nil? || msg_event[:message][:text].nil?
+      if msg_event[:postback]
+        msg_data = send_text_message_data("postback received: #{msg_event[:postback]}")
+        send_message(sender, msg_data)
+      elsif msg_event[:message] && msg_event[:message][:text]
         text = msg_event[:message][:text]
-        send_message(sender, text)
+        msg_data = send_text_message_data(text)
+        msg_data = generic_message_data if text.downcase == 'generic'
+        send_message(sender, msg_data)
       end
     end
     head 200
   end
 
-  def send_message(sender, text)
-    message_data = {
-      text: text
-    }
+  def send_message(sender, message_data)
     payload = {
       recipient: {
         id: sender
@@ -29,5 +31,54 @@ class FacebookController < ActionController::Base
     }
     RestClient.post("https://graph.facebook.com/v2.6/me/messages?access_token=#{Facebook::Messenger.config.access_token}",
                     payload.to_json, content_type: :json, accept: :json)
+  end
+
+  def send_text_message_data(text)
+    {
+      text: text
+    }
+  end
+
+  def generic_message_data
+    {
+      attachment:
+        {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements:
+              [
+                {
+                  title: 'First card',
+                  subtitle: 'Element #1 of an hscroll',
+                  image_url: 'http://messengerdemo.parseapp.com/img/rift.png',
+                  buttons:
+                    [
+                      {
+                        type: 'web_url',
+                        url: 'https://www.messenger.com/',
+                        title: 'Web url'
+                      },
+                      {
+                        type: 'postback',
+                        title: 'Postback',
+                        payload: 'Payload for first element in a generic bubble'
+                      }
+                    ]
+                },
+                {
+                  title: 'Second card',
+                  subtitle: 'Element #2 of an hscroll',
+                  image_url: 'http://messengerdemo.parseapp.com/img/gearvr.png',
+                  buttons: [{
+                    type: 'postback',
+                    title: 'Postback',
+                    payload: 'Payload for second element in a generic bubble'
+                  }]
+                }
+              ]
+            }
+          }
+      }
   end
 end
