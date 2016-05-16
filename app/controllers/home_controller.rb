681d3c7d46fd8f90b0fd1817db17b197035ce8f9
ccleung <clemsquared@gmail.com>
2016-05-15 22:19:19 -0400

    remove pry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
-    binding.pry
-    @products
   end
 end
