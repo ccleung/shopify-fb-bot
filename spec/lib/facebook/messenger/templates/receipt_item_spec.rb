7a2e883d214f479bf082666e93a8108ee17f3cdc
ccleung <clemsquared@gmail.com>
2016-07-12 23:53:16 -0400

    specs moved to gem

--- spec/lib/facebook/messenger/templates/receipt_item_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Fb::Messenger::Template::ReceiptItem do
-  context '#template' do
-    it 'contains the proper attributes' do
-      expect(subject.template).to include(:title, :subtitle, :quantity, :price, :currency, :image_url)
-    end
-  end
-end
